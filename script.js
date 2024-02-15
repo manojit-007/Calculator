489ceea6bc3277d7463fd1ddf99af5d016ab48a4
manojit-007 <manojitbhowmick.prof.07@gmail.com>
2024-02-15 14:59:21 +0530

    Done

---
+++ script.js
@@ -0,0 +1,39 @@
+let buttons = document.querySelectorAll(".buttons button");
+let display = document.querySelector(".display");
+const operators = ["%", "*", "/", "+", "-"];
+let result = "";
+
+let calculate = (btn) => {
+    if (btn === "=" && result !== "") {
+        result = eval(result.replace("%", "/100"));
+        display.value = result;
+    } else if (btn === "AC") {
+        result = "";
+    } else if (btn === "DEL") {
+        result = result.slice(0, -1);
+    } else if (btn === "SQR") {
+        result = Math.pow(Number(display.value), 2);
+    } else if (btn === "CUBE") {
+        result = Math.pow(Number(display.value), 3);
+    } else if (btn === "ROOT") {
+        result = Math.sqrt(display.value);
+    } else if (btn === "NEG") {
+        // result = `(${-display.value})`;
+        result = -Number(display.value);
+    } else {
+        if (result === "" && operators.includes(btn)) return;
+        result += btn;
+    }
+    display.value = result;
+};
+
+buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        calculate(event.target.dataset.value);
+    });
+});
+
+{/* <button class="operator" data-value="SQR">X <sup>2</sup></button>
+<button class="operator" data-value="CUBE">X <sup>3</sup></button>
+<button class="operator" data-value="ROOT">√ </button>
+<button class="operator" data-value="NEG">+/-</button> */}
